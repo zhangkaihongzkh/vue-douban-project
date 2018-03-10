ff688a7db9465f2a8fcfe97a4cad4073b1f71a2b
zkhzz <953628520@qq.com>
2018-03-10 21:29:30 +0800

    test-uifeature

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,12 @@
       <el-button type="danger">危险按钮</el-button>
     </div>
 
+    <el-button type="primary" icon="el-icon-edit"></el-button>
+<el-button type="primary" icon="el-icon-share"></el-button>
+<el-button type="primary" icon="el-icon-delete"></el-button>
+<el-button type="primary" icon="el-icon-search">搜索</el-button>
+<el-button type="primary">上传<i class="el-icon-upload el-icon--right"></i></el-button>
+
   </div>
 </template>
 
